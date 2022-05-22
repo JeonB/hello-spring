e4a302c712257c4cc40003e5d63bcbdc612f3fcd
JeonB <newjeonb@gmail.com>
2022-05-23 06:31:12 +0900

    스프링 0523/spring 완료

--- src/main/java/hello/hellospring/serivice/MemberService.java
+++ src/main/java/hello/hellospring/serivice/MemberService.java
@@ -6,11 +6,12 @@ import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 
-
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -24,10 +25,9 @@ public class MemberService {
     * 회원 가입
     * */
     public Long join(Member member){
-        //같은 이름이 있는 중복 회원x
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -42,8 +42,7 @@ public class MemberService {
     * 전체 회원 조회
     * */
     public List<Member> findMembers(){
-        return memberRepository.findAll();
-
+            return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId){
